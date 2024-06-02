c9dd91bc56c66fc04f846aebeaf9bb01a2294df2
Alioth4J <Alioth4J@proton.me>
2024-06-02 15:02:34 +0800

    增加Redis作为缓存

---
+++ src/main/java/com/alioth4j/chatgpt3p5o/config/RedisConfig.java
@@ -0,0 +1,65 @@
+package com.alioth4j.chatgpt3p5o.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * Redis配置类
+ * @author Alioth4J
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    public static final String VALUE = "ChatGPT-3.5o";
+    public static final String KEY_HISTORY = "'history:' + T(com.alioth4j.chatgpt3p5o.util.UserIdUtil).getUserId() + ':' + #{id}";
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisSerializer<Object> redisSerializer = redisSerializer();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(redisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisSerializer<Object> redisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+        return serializer;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
+        // 设置Redis缓存有效期为5天
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer()))
+                .entryTtl(Duration.ofDays(5));
+        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
+
+}
--- src/main/java/com/alioth4j/chatgpt3p5o/service/impl/HistoryServiceImpl.java
+++ src/main/java/com/alioth4j/chatgpt3p5o/service/impl/HistoryServiceImpl.java
@@ -1,11 +1,15 @@
 package com.alioth4j.chatgpt3p5o.service.impl;
 
+import com.alioth4j.chatgpt3p5o.config.RedisConfig;
 import com.alioth4j.chatgpt3p5o.mapper.HistoryMapper;
 import com.alioth4j.chatgpt3p5o.pojo.entity.History;
 import com.alioth4j.chatgpt3p5o.service.HistoryService;
 import com.alioth4j.chatgpt3p5o.util.UserIdUtil;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,10 @@ public class HistoryServiceImpl implements HistoryService {
     @Autowired
     private HistoryMapper historyMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    // 增 先不放到缓存中
     @Override
     public void create(String question, String answer) {
         Long userId = UserIdUtil.getUserId();
@@ -27,18 +35,7 @@ public class HistoryServiceImpl implements HistoryService {
     }
 
     @Override
-    public History list(Long id) {
-        return historyMapper.list(id);
-    }
-
-    @Override
-    public List<History> listAll(Integer pageNum, Integer pageSize) {
-        PageHelper.startPage(pageNum, pageSize);
-        Long userId = UserIdUtil.getUserId();
-        return historyMapper.listAll(userId);
-    }
-
-    @Override
+    @CacheEvict(value = RedisConfig.VALUE, key = RedisConfig.KEY_HISTORY)
     public void delete(Long id) {
         historyMapper.delete(id);
     }
@@ -46,7 +43,34 @@ public class HistoryServiceImpl implements HistoryService {
     @Override
     public void deleteAll() {
         Long userId = UserIdUtil.getUserId();
+        // 为了删缓存，先查得所有
+        List<History> historyList = historyMapper.listAll(userId);
+        for (History history : historyList) {
+            Long id = history.getId();
+            redisTemplate.delete("history:" + userId + id);
+        }
+        // 删数据库
         historyMapper.deleteAll(userId);
+    }
+
+    @Override
+    @Cacheable(value = RedisConfig.VALUE, key = RedisConfig.KEY_HISTORY)
+    public History list(Long id) {
+        return historyMapper.list(id);
+    }
+
+    @Override
+    public List<History> listAll(Integer pageNum, Integer pageSize) {
+        // 直接去数据库查，不查缓存（缓存可能不全）
+        PageHelper.startPage(pageNum, pageSize);
+        Long userId = UserIdUtil.getUserId();
+        List<History> historyList = historyMapper.listAll(userId);
+        // 放到缓存中
+        for (History history : historyList) {
+            Long id = history.getId();
+            redisTemplate.opsForValue().setIfAbsent("history:" + userId + id, history);
+        }
+        return historyList;
     }
 
 }
